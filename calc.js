d2dceb12b425dd1aa0cd556d3b61ab77c344a107
sphatak21 <siddphatak@gmail.com>
2019-01-21 22:53:31 -0500

    almost done

--- calc.js
+++ calc.js
@@ -6,6 +6,7 @@ var decimalPoint=true;
 var decimalNum=false;
 var numlength=1;
 var ans='';
+var ansNum=false;
 function fourfunction(operation) {
   if(operation == '+' && symbol==true) {
     expression+="+";
@@ -13,6 +14,7 @@ function fourfunction(operation) {
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=false;
     numlength=1;
+    ansNum=false
   }
   if(operation=='x' && symbol==true){
     expression+='*';
@@ -20,6 +22,7 @@ function fourfunction(operation) {
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=false;
 	numlength=1;
+	ansNum=false
   }
   if(operation == '/' && symbol==true) {
     expression+="/";
@@ -27,6 +30,7 @@ function fourfunction(operation) {
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=false;
     numlength=1;
+    ansNum=false
   }
   if(operation=='-' && symbol==true){
     expression+='-';
@@ -34,13 +38,15 @@ function fourfunction(operation) {
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=false;
     numlength=1;
+    ansNum=false
   }
   if(operation == '%' && symbol==true){
     expression+='*.01'
     displayExpression+="%";
     document.getElementById('addition').innerHTML=displayExpression;
+    ansNum=false
   }
-  if (operation=='.' && symbol==true && decimalNum==false){
+  if (operation=='.' && symbol==true && decimalNum==false && ansNum==false){
     expression+='.'+'0';
     displayExpression+='.'+'0';
     document.getElementById('addition').innerHTML=displayExpression;
@@ -48,9 +54,24 @@ function fourfunction(operation) {
     decimalZero=true;
     decimalPoint=true;
     decimalNum=true;
-
   }
-
+  if(operation=='x^2' && symbol==true){
+	displayExpression+='^2';
+	expression+='**2';
+    document.getElementById('addition').innerHTML=displayExpression;
+  }
+  if(operation=='x^3' && symbol==true){
+	displayExpression+='^3';
+	expression+='**3';
+	document.getElementById('addition').innerHTML=displayExpression;
+  }
+  if(operation=='x^y' && symbol==true){
+	displayExpression+='^';
+	expression+='**';
+	document.getElementById('addition').innerHTML=displayExpression;
+	ansNum=false;
+	symbol=false;
+  }
 }
 function numbers(operation){
   if(decimalZero==true){
@@ -62,12 +83,36 @@ function numbers(operation){
     displayExpression=q.join('')
     decimalZero=false;
   }
+
+  if(operation=='del') {
+	  let z=displayExpression.split('');
+	  let r=expression.split('');
+	  if(Number.isInteger(Number(z[z.length-1]))||z[z.length-1]=='x'||z[z.length-1]=='/'||z[z.length-1]||z[z.length-1]=='-'||z[z.length-1]=='+'||z[z.length-1]=='%'){
+	  	temp=r.pop();
+	  }
+	  if(z[z.length-1]=='^'){
+	    temp=z.pop();
+	    temp+=r.pop();
+	    temp+=r.pop();
+	  }
+	  expression=r.join('');
+	  displayExpression=z.join('')
+	  document.getElementById('addition').innerHTML=displayExpression;
+	  if(temp=='*'||temp.length-1=='-'||temp== '/' ||temp=='%'||temp=='**'||temp=='+'){
+		symbol=true;
+	  }
+	  if(displayExpression[displayExpression.length-1]=='x'||displayExpression[displayExpression.length-1]=='-'||displayExpression[displayExpression.length-1]== '/' ||displayExpression[displayExpression.length-1]=='%'||displayExpression[displayExpression.length-1]=='^'||displayExpression[displayExpression.length-1]=='+'){
+		symbol=false;
+	  }
+  }
   if (operation=="negation"){
-	  let z=displayExpression.split('')
-	  expression+="*-1";
+	  let z=displayExpression.split('');
+	  let r=expression.split('');
+	  
 	  let x=true;
+	  let y=true;
 	  for(let i=displayExpression.length-1;i>=0;i--){    
-		  if((displayExpression[i]=='x'||displayExpression[i]=='/'||displayExpression[i]=='+'||displayExpression=='-') && x==true){  
+		  if((displayExpression[i]=='x'||displayExpression[i]=='/'||displayExpression[i]=='+'||displayExpression=='-'||displayExpression[i]=='^') && x==true){  
 			  x=false;
 			  var tempArray=[];
 			  for(let j=0; j<=i; j++){
@@ -75,82 +120,110 @@ function numbers(operation){
 				  tempArray[j]=temp;
 			  }
 			  z.unshift('-');
-			  console.log(z);
 			  let arrayLength=tempArray.length;
 			  for(let k=0;k<arrayLength;k++){
 				let temp2=tempArray.pop();
 				z.unshift(temp2);
-				console.log(z);
-			  }
-		  }	
+			  }			  
+		  }
+	  }
+	  for(let n=expression.length-1; n>=0;n--){
+		if((expression[n]=='*'||expression[n]=='-'||expression[n]=='+'||expression[n]=='/')&& y==true){
+			y=false;
+			var tempArray2=[];
+			for(let m=0;m<=n;m++){
+				let temp=r.shift();
+				tempArray2[m]=temp;
+				console.log(tempArray);
+			}
+			console.log("1.")
+			console.log(r)
+			r.unshift('-');
+			console.log("2.")
+			console.log(r);
+			let arrayLength2=tempArray2.length;
+			for(let s=0;s<arrayLength2;s++){
+				let temp2=tempArray2.pop();
+				r.unshift(temp2);
+			}
 		}
-	  displayExpression=z.join('');
- 	  document.getElementById('addition').innerHTML=displayExpression;
+	  }
+	if(x==true){
+	  z.unshift('-');
+	}
+	if(y==true){
+	  r.unshift('-');
+	}
+	displayExpression=z.join('');
+ 	expression=r.join('');
+ 	document.getElementById('addition').innerHTML=displayExpression;
   }
-  if (operation=='0' && numlength<=9){
+
+  
+  if (operation=='0' && numlength<=9 && ansNum==false){
     expression+='0';
     displayExpression+='0';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='1'&& numlength<=9){
+  if (operation=='1'&& numlength<=9 && ansNum==false){
     expression+='1';
     displayExpression+='1';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='2'&& numlength<=9){
+  if (operation=='2'&& numlength<=9 && ansNum==false){
     expression+='2';
     displayExpression+='2';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='3'&& numlength<=9){
+  if (operation=='3'&& numlength<=9 && ansNum==false){
     expression+='3';
     displayExpression+='3';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;    
   }
-  if (operation=='4'&& numlength<=9){
+  if (operation=='4'&& numlength<=9 && ansNum==false){
     expression+='4';
     displayExpression+='4';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='5'&& numlength<=9){
+  if (operation=='5'&& numlength<=9 && ansNum==false){
     expression+='5';
     displayExpression+='5';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='6'&& numlength<=9){
+  if (operation=='6'&& numlength<=9 && ansNum==false){
     expression+='6';
     displayExpression+='6';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='7'&& numlength<=9){
+  if (operation=='7'&& numlength<=9 && ansNum==false){
     expression+='7';
     displayExpression+='7';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='8'&& numlength<=9){
+  if (operation=='8'&& numlength<=9 && ansNum==false){
     expression+='8';
     displayExpression+='8';
     document.getElementById('addition').innerHTML=displayExpression;
     symbol=true;
     numlength++;
   }
-  if (operation=='9'&& numlength<=9){
+  if (operation=='9'&& numlength<=9 && ansNum==false){
     expression+='9';
     displayExpression+='9';
     document.getElementById('addition').innerHTML=displayExpression;
@@ -163,6 +236,7 @@ function numbers(operation){
   document.getElementById('addition').innerHTML=displayExpression;
   symbol=true;
   decimalNum=false;
+  ansNum=true;
   }
 }
 function AC(){
@@ -180,13 +254,17 @@ function equals(operation){
   let finalExpression=eval(expression);
   if (operation== '='){
     if (finalExpression==Infinity){
-      document.getElementById('addition').innerHTML="Error";
+      document.getElementById('addition').innerHTML="U Hav Made A Grave Mistake";
     }
     else if(finalExpression>=1000000000){
       let exponent=finalExpression.toString().length-1;
       document.getElementById('addition').innerHTML=finalExpression/(10**(exponent))+'e'+exponent;
 
     }
+    else if(finalExpression<=-1000000000){
+	  let exponent=finalExpression.toString().length-2;
+	  document.getElementById('addition').innerHTML=finalExpression/(10**(exponent))+'e'+exponent;
+	}		
     else{
       document.getElementById('addition').innerHTML=finalExpression.toLocaleString();
     }
